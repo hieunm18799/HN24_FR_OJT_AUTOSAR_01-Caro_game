3e6ab74f7b00449b3c078d748ef97b97487caa40
VuQuan <115912991+quanvu20702@users.noreply.github.com>
2024-08-27 16:57:37 +0700

    Add files via upload

---
+++ src/view/top_screen.h
@@ -0,0 +1,10 @@
+#ifndef TOP_SCREEN_H
+#define TOP_SCREEN_H
+
+void drawTopNotLoginScreen();
+void drawTopLoginedScreen();
+void processLoginScreen();
+void enterSignupCredentials(char *username, char *password, char *reenterPassword);
+void enterCredentials(char *username, char *password);
+
+#endif
