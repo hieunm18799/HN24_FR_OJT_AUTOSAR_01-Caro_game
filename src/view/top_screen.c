1b8fdf3f61119840468a0fe634f452e2767b040a
VuQuan <115912991+quanvu20702@users.noreply.github.com>
2024-08-28 16:41:07 +0700

    Add files via upload

--- src/view/top_screen.c
+++ src/view/top_screen.c
@@ -1,6 +1,16 @@
+#include <stdio.h>
 #include <conio.h>
 #include <windows.h>
-#include <top_screen.h>
+#include "top_screen.h"
+
+COORD MousePos; // Biến lưu trữ vị trí chuột
+int Click_flag = 0; // Cờ để xác định xem đã click chuột hay chưa
+int currentScreen = 0; 
+char signin_username[50];
+char signin_password[50];
+char signup_username[50];
+char signup_password[50];
+char signup_reenterPassword[50];
 
 void gotoxy(int x, int y) {
     COORD coord;
