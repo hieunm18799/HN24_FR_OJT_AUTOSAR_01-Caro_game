65367fdf8d48acb408c84030fb8ac30823116aab
Loan <118834210+quabongbong@users.noreply.github.com>
2024-08-29 16:53:55 +0700

    Create admindashboard.c, udate top_screen.h, update dashboard.c

---
+++ src/view/admin_screen/admindashboard.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <windows.h>
+#include "vFunction.h"
+
+
+void frameAdminScreen() {
+	system("cls");
+	gotoxy(27, 1);
+	gotoxy(15, 3);
+	printf("ADMIN DASHBOARD");
+	for (int i = 2; i <= 7; i++) {
+		if (i <= 4) {
+			gotoxy(10, 4 * i - 2);
+			printf("[");
+			gotoxy(33, 4 * i - 2);
+			printf("]");
+		}
+	}
+	gotoxy(13, 6);  printf("User Management");
+	gotoxy(13, 10); printf("Replay Management");
+	gotoxy(17, 14); printf("Back");
+	currentScreen = 5;
+}
+
+void openAdminScreen(char* role, char* username) {
+	if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
+		// chuyen sang user management
+	}
+	else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
+		//chuyen sang man hinh replay
+	}
+	else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
+		dashboard(role, username);
+	}
+}
