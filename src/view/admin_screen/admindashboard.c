2a2ca26d9bd9e02cc07bff07e1376143e4a1b893
Loan <118834210+quabongbong@users.noreply.github.com>
2024-08-29 21:29:32 +0700

    Update admindashboard.c

--- src/view/admin_screen/admindashboard.c
+++ src/view/admin_screen/admindashboard.c
@@ -1,35 +1,87 @@
 #include <stdio.h>
 #include <windows.h>
-#include "vFunction.h"
+#include "top_screen.h"
 
+int currentScreen = 0;
 
-void frameAdminScreen() {
+void frameDashboardAdmin(char* role, char* username) {
 	system("cls");
 	gotoxy(27, 1);
-	gotoxy(15, 3);
-	printf("ADMIN DASHBOARD");
+	printf("%s", username);
+	gotoxy(17, 3);
+	printf("CARO GAME");
 	for (int i = 2; i <= 7; i++) {
-		if (i <= 4) {
+		if (i <= 5) {
 			gotoxy(10, 4 * i - 2);
 			printf("[");
 			gotoxy(33, 4 * i - 2);
 			printf("]");
 		}
 	}
-	gotoxy(13, 6);  printf("User Management");
-	gotoxy(13, 10); printf("Replay Management");
-	gotoxy(17, 14); printf("Back");
-	currentScreen = 5;
+	gotoxy(17, 6);  printf("Play game");
+	gotoxy(17, 10); printf("Replay list");
+	gotoxy(13, 14); printf("Admin management");
+	gotoxy(17, 18); printf("Sign out");
+	
+	currentScreen = 21;
 }
 
-void openAdminScreen(char* role, char* username) {
-	if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
-		// chuyen sang user management
-	}
-	else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
-		//chuyen sang man hinh replay
-	}
-	else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
-		dashboard(role, username);
-	}
-}
+ void frameDashboardUser(char *role, char *username) {
+	 system("cls");
+	 gotoxy(27, 1);
+	 printf("%s", username);
+	 gotoxy(17, 3);
+	 printf("CARO GAME");
+	 for (int i = 2; i <= 7; i++) {
+		 if (i <= 4) {
+			 gotoxy(10, 4 * i - 2);
+			 printf("[");
+			 gotoxy(33, 4 * i - 2);
+			 printf("]");
+		 }
+	 }
+	 gotoxy(17, 6);  printf("Play game");
+	 gotoxy(17, 10); printf("Replay list");
+	 gotoxy(17, 14); printf("Sign out");
+	 currentScreen = 22;
+ }
+ 
+ //open new screen from adminScreen
+ void openAdmin(char *role, char *username) {
+	 if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 //man hinh playgame 
+	 }
+	 else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 //chuyen sang man hinh replay 
+	 }
+	 else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 frameAdminScreen();
+	 }
+	 else if (MousePos.Y == 18 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 drawInitialUI();
+	 }
+ }
+
+ //open new screen from userCreen
+ void openUser(char* role, char* username) {
+	 if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 //chuyen sang man hình playgame
+	 }
+	 else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 //chuyen sang man hinh replay
+	 }
+
+	 else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
+		 drawInitialUI();
+	 }
+ }
+
+ void dashboard(char *role, char *username) {
+	 if (strcmp(role, "user") == 0) {
+		 frameDashboardUser( role, username);
+	 } else if (strcmp(role, "admin") == 0) {
+		 frameDashboardAdmin(role, username);
+	 } else {
+		 printf("ERROR");
+	 }
+ }
