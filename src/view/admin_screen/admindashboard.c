8d33c0c25a18ed71cbdbc5eb9bd403dd23ffa703
Loan <118834210+quabongbong@users.noreply.github.com>
2024-08-29 21:31:24 +0700

    Update admindashboard.c

--- src/view/admin_screen/admindashboard.c
+++ src/view/admin_screen/admindashboard.c
@@ -2,86 +2,34 @@
 #include <windows.h>
 #include "top_screen.h"
 
-int currentScreen = 0;
 
-void frameDashboardAdmin(char* role, char* username) {
+void frameAdminScreen() {
 	system("cls");
 	gotoxy(27, 1);
-	printf("%s", username);
-	gotoxy(17, 3);
-	printf("CARO GAME");
+	gotoxy(15, 3);
+	printf("ADMIN DASHBOARD");
 	for (int i = 2; i <= 7; i++) {
-		if (i <= 5) {
+		if (i <= 4) {
 			gotoxy(10, 4 * i - 2);
 			printf("[");
 			gotoxy(33, 4 * i - 2);
 			printf("]");
 		}
 	}
-	gotoxy(17, 6);  printf("Play game");
-	gotoxy(17, 10); printf("Replay list");
-	gotoxy(13, 14); printf("Admin management");
-	gotoxy(17, 18); printf("Sign out");
-	
-	currentScreen = 21;
+	gotoxy(13, 6);  printf("User Management");
+	gotoxy(13, 10); printf("Replay Management");
+	gotoxy(17, 14); printf("Back");
+	currentScreen = 5;
 }
 
- void frameDashboardUser(char *role, char *username) {
-	 system("cls");
-	 gotoxy(27, 1);
-	 printf("%s", username);
-	 gotoxy(17, 3);
-	 printf("CARO GAME");
-	 for (int i = 2; i <= 7; i++) {
-		 if (i <= 4) {
-			 gotoxy(10, 4 * i - 2);
-			 printf("[");
-			 gotoxy(33, 4 * i - 2);
-			 printf("]");
-		 }
-	 }
-	 gotoxy(17, 6);  printf("Play game");
-	 gotoxy(17, 10); printf("Replay list");
-	 gotoxy(17, 14); printf("Sign out");
-	 currentScreen = 22;
- }
- 
- //open new screen from adminScreen
- void openAdmin(char *role, char *username) {
-	 if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 //man hinh playgame 
-	 }
-	 else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 //chuyen sang man hinh replay 
-	 }
-	 else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 frameAdminScreen();
-	 }
-	 else if (MousePos.Y == 18 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 drawInitialUI();
-	 }
- }
-
- //open new screen from userCreen
- void openUser(char* role, char* username) {
-	 if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 //chuyen sang man hình playgame
-	 }
-	 else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 //chuyen sang man hinh replay
-	 }
-
-	 else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
-		 drawInitialUI();
-	 }
- }
-
- void dashboard(char *role, char *username) {
-	 if (strcmp(role, "user") == 0) {
-		 frameDashboardUser( role, username);
-	 } else if (strcmp(role, "admin") == 0) {
-		 frameDashboardAdmin(role, username);
-	 } else {
-		 printf("ERROR");
-	 }
- }
+void openAdminScreen(char* role, char* username) {
+	if (MousePos.Y == 6 && MousePos.X >= 10 && MousePos.X <= 33) {
+		// chuyen sang user management
+	}
+	else if (MousePos.Y == 10 && MousePos.X >= 10 && MousePos.X <= 33) {
+		//chuyen sang man hinh replay
+	}
+	else if (MousePos.Y == 14 && MousePos.X >= 10 && MousePos.X <= 33) {
+		dashboard(role, username);
+	}
+}
